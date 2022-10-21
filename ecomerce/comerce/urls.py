c1076646736ce398865999cdc96de089284dfdec
marcos echeverria <marcosalbertoecheverria@gmail.com>
2022-10-21 09:26:03 -0300

    navbar

--- ecomerce/comerce/urls.py
+++ ecomerce/comerce/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('contact', views.contact, name="contact"),
+    path('link', views.link, name="link"),
 ]
--- ecomerce/comerce/views.py
+++ ecomerce/comerce/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-
 # Create your views here.
 def home(request):
     template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+def contact(request):
+    template = loader.get_template('contact.html')
+    return HttpResponse(template.render())
+
+def link(request):
+    template = loader.get_template('link.html')
     return HttpResponse(template.render())
